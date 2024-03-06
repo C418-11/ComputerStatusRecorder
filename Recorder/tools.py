688f9e20b7419fd1d72869f5e03b4932087ad06b
C418____11 <553515788@qq.com>
2024-03-06 17:07:26 +0800

    添加了更多保存格式支持

--- Recorder/tools.py
+++ Recorder/tools.py
@@ -2,43 +2,86 @@
 # cython: language_level = 3
 
 __author__ = "C418____11 <553515788@qq.com>"
-__version__ = "0.0.1Dev"
+__version__ = "0.0.2Dev"
 
 import os
 import struct
 import time
+import warnings
 from enum import StrEnum
 
 
 class PackFmt(StrEnum):
-    Int = 'Q'
-    Float = 'd'
+    Char = 'c'
+    SignedChar = 'b'
+    UnsignedChar = 'B'
+    Bool = '?'
+    Short = 'h'
+    UnsignedShort = 'H'
+    Int = 'i'
+    UnsignedInt = 'I'
+    Long = 'l'
+    UnsignedLong = 'L'
+    LongLong = 'q'
+    UnsignedLongLong = 'Q'
+    HalfFloat = 'e'
+    Float = 'f'
+    Double = 'd'
 
 
-def pack_timestamp_que(que, fmt=PackFmt.Int):
+def pack_timestamp_que(que, *fmt_tuple, warn: bool = True):
     """
     :param que: queue[time.time_ns(), value]
-    :param fmt: struct.pack(fmt, value)
+    :param fmt_tuple: Iterable[PackFmt | str | "fmt1 fmt2 fmt3"]]
+    :param warn: 是否警告非标准格式
     :return:
     """
 
-    if not isinstance(fmt, PackFmt):
-        raise TypeError("fmt must be PackFmt")
+    return pack_list(
+        que,
+        PackFmt.UnsignedLongLong,
+        *fmt_tuple,
+        warn=warn
+    )
 
-    file_head = b'Q|' + fmt.value.encode() + b'\n'  # 文件头记录文件解码格式
 
-    ls = [file_head]
+def pack_list(_list, *fmt_tuple, warn: bool = True):
+    """
+    :param _list: list[...]
+    :param fmt_tuple: Iterable[PackFmt | str | "fmt1 fmt2 fmt3"]]
+    :param warn: 是否警告非标准格式
+    :return:
+    """
 
-    for timestamp, value in que:
+    def _parse_fmt(_tuple):
+        if len(_tuple) == 1:
+            _fmt = _tuple[0]
+            if type(_fmt) is str:
+                return [_fmt]
 
-        if type(timestamp) is not int:
-            raise TypeError("timestamp must be int")
+        return [_f.value if type(_f) is PackFmt else _f for _f in fmt_tuple]
 
-        byte = struct.pack('Q', timestamp)
-        byte += struct.pack(fmt, value)
-        ls.append(byte)
+    fmt_ls: list[str] = _parse_fmt(fmt_tuple)
 
-    return ls
+    for _ in fmt_ls:
+        if _ not in PackFmt and warn:
+            warnings.warn(
+                f"fmt {_} is not in PackFmt",
+                RuntimeWarning,
+                stacklevel=2
+            )
+
+    file_head = b'|'.join([_.encode() for _ in fmt_ls]) + b'\n'  # 文件头记录文件解码格式
+    ret_ls = [file_head]
+
+    for value in _list:
+        byte = b''
+        for fmt, v in zip(fmt_ls, value):
+            byte += struct.pack(fmt, v)
+
+        ret_ls.append(byte)
+
+    return ret_ls
 
 
 def time_str(fmt, timestamp: int):
